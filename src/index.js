65b2b36666677b17cc50a0b18f0282d46c0d3248
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2023-10-25 22:17:05 +0100

    sidebar v2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <ChakraProvider>
     <App />
-   </ChakraProvider>
 );
