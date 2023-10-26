2b678ef169e2bdcbcca83fa362c3c6c761fb329c
taoufik <ultrasarabic@gmail.com>
2023-10-26 20:54:54 +0100

    drilling items

--- src/components/filters/drilling/Drilling.jsx
+++ src/components/filters/drilling/Drilling.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 import { useState } from 'react';
 import drillData from "../../../data/drilling.json"
 import "./drilling.css"
-import { Accordion } from '@chakra-ui/react';
-import { AccordionDetails, AccordionSummary, Typography } from '@mui/material';
-import { ArrowCircleUpRounded, ArrowDropDown, ArrowDropDownCircleRounded, ExpandMore } from '@mui/icons-material';
+import {ArrowDropDownCircleRounded} from '@mui/icons-material';
 const Drilling = () => {
     const [selected, setselected] = useState(false)
-    const [activeIndex, setActiveIndex] = useState(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+
 
     const toggleAccordion = (index) => {
       setselected(prev => activeIndex !== index || !prev)
       setActiveIndex(index)
     };
-
+ 
   return (
     <div>
     <div className="drilling-container">
@@ -22,20 +21,20 @@ const Drilling = () => {
                 <span className='drilling-header'><span></span>{el.title}<span></span></span>
                 <div className={`drilling-items open`}>
                   {el.content.map((el,i)=> 
-                <div className='drilling-item'>
+                <div  className={`drilling-item`}>
                   {el}
                 </div> )}
                 </div>
             </div>)}
     {drillData.second.map((el,i)=>
             <div className='drilling-menu'>
-                <span onClick={() => toggleAccordion(i)} className={`drilling-header ${activeIndex === i && 'open'}`}>  
+                <span onClick={() => toggleAccordion(i)} className={`drilling-header ${selected&&activeIndex === i && 'open'}`}>  
                   <span></span> <span>{el.title}</span>
-                  {selected?<ArrowCircleUpRounded/> : <ArrowDropDownCircleRounded/>} 
+                  {selected&&activeIndex === i ?<ArrowDropDownCircleRounded className='rotate'/> : <ArrowDropDownCircleRounded/>} 
                   </span>
                 <div className={`drilling-items ${selected&&activeIndex === i && 'open'}`}>
-                  {el.content.map((el,i)=> 
-                <div className="drilling-item">
+                  {el.content.map((el,j)=> 
+                <div className={`drilling-item`}>
                   {el}
                 </div> )}
                 </div>
