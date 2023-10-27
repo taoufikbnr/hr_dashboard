cd03421a41f9bc59cbe6321220b56b2c59366bf2
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2023-10-27 09:40:22 +0100

    selected filters bg

--- src/components/filters/drilling/Drilling.jsx
+++ src/components/filters/drilling/Drilling.jsx
@@ -6,13 +6,21 @@ import {ArrowDropDownCircleRounded} from '@mui/icons-material';
 const Drilling = () => {
     const [selected, setselected] = useState(false)
     const [activeIndex, setActiveIndex] = useState(0);
+    const [selectedIndexes, setSelectedIndexes] = useState({ i: null, j: null });
 
 
     const toggleAccordion = (index) => {
       setselected(prev => activeIndex !== index || !prev)
       setActiveIndex(index)
     };
- 
+    const handleItemClick = (i, j) => {
+      if (selectedIndexes.i === i && selectedIndexes.j === j) {
+        setSelectedIndexes({ i: null, j: null });
+      } else {
+        setSelectedIndexes({ i, j });
+      }
+    };
+    console.log(selectedIndexes);
   return (
     <div>
     <div className="drilling-container">
@@ -20,8 +28,8 @@ const Drilling = () => {
             <div className='drilling-menu'>
                 <span className='drilling-header'><span></span>{el.title}<span></span></span>
                 <div className={`drilling-items open`}>
-                  {el.content.map((el,i)=> 
-                <div  className={`drilling-item`}>
+                  {el.content.map((el,j)=> 
+                <div onClick={() => handleItemClick(i+2,j)} className={`drilling-item ${selectedIndexes.i === i+2 && selectedIndexes.j === j ? 'selected' : ''}`}>
                   {el}
                 </div> )}
                 </div>
@@ -34,7 +42,7 @@ const Drilling = () => {
                   </span>
                 <div className={`drilling-items ${selected&&activeIndex === i && 'open'}`}>
                   {el.content.map((el,j)=> 
-                <div className={`drilling-item`}>
+                <div onClick={() => handleItemClick(i, j)} className={`drilling-item ${selectedIndexes.i === i && selectedIndexes.j === j ? 'selected' : ''}`}>
                   {el}
                 </div> )}
                 </div>
