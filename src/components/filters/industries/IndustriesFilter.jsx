663a63247a841c505ed5ce611413b16b449c0b02
taoufik-io <taoufik.bennour@esprit.tn>
2023-10-28 19:04:40 +0100

    drilling/industries/clients multiselection

--- src/components/filters/industries/IndustriesFilter.jsx
+++ src/components/filters/industries/IndustriesFilter.jsx
@@ -2,28 +2,21 @@ import "./industriesFilter.css"
 import indusData from "../../../data/industries.json"
 import { useState } from "react"
 const IndustriesFilter = () => {
-    const [selected, setselected] = useState(false)
-    const [currentIndex, setCurrentIndex] = useState([]);
 
-    const handleClick=(i)=>{
-        setselected(prev => currentIndex !== i || !prev)
-        const newSelectedItems = [...currentIndex];
+  const [selectedIndustries, setSelectedIndustries] = useState([]);
 
-        const index = newSelectedItems.findIndex((item) => item.i === i);
-  
-        if (index === -1) {
-          newSelectedItems.push({ i});
-        } else {
-          newSelectedItems.splice(index, 1);
-        }
-  
-        setCurrentIndex(newSelectedItems);
-    }
+  const handleItemClick = (industry) => {
+      if (selectedIndustries.includes(industry)) {
+          setSelectedIndustries(selectedIndustries.filter((item) => item !== industry));
+      } else {
+          setSelectedIndustries([...selectedIndustries, industry]);
+      }
+  };
   return (
     <>
     <div className="industries-container">
     {indusData.industries.map((el,i)=>
-            <div onClick={()=>handleClick(i)} className={`industries-item ${currentIndex.some((item) => item.i === i)&&selected&&"selected"}`}>
+            <div key={i} onClick={()=>handleItemClick(el)} className={`industries-item ${selectedIndustries.includes(el)&&"selected"}`}>
                 {el}
             </div>)}
     </div>
