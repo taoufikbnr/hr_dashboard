1d6beaf2b1b63cef790e85fc12e3fe4b9be9f8be
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2023-10-28 12:11:37 +0100

    industries filter multiselection

--- src/components/filters/industries/IndustriesFilter.jsx
+++ src/components/filters/industries/IndustriesFilter.jsx
@@ -3,17 +3,27 @@ import indusData from "../../../data/industries.json"
 import { useState } from "react"
 const IndustriesFilter = () => {
     const [selected, setselected] = useState(false)
-    const [currentIndex, setCurrentIndex] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState([]);
 
     const handleClick=(i)=>{
         setselected(prev => currentIndex !== i || !prev)
-        setCurrentIndex(i)
+        const newSelectedItems = [...currentIndex];
+
+        const index = newSelectedItems.findIndex((item) => item.i === i);
+  
+        if (index === -1) {
+          newSelectedItems.push({ i});
+        } else {
+          newSelectedItems.splice(index, 1);
+        }
+  
+        setCurrentIndex(newSelectedItems);
     }
   return (
     <>
     <div className="industries-container">
     {indusData.industries.map((el,i)=>
-            <div onClick={()=>handleClick(i)} className={`industries-item ${currentIndex===i&&selected&&"selected"}`}>
+            <div onClick={()=>handleClick(i)} className={`industries-item ${currentIndex.some((item) => item.i === i)&&selected&&"selected"}`}>
                 {el}
             </div>)}
     </div>
