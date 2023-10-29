881f4132f114618216cede69734143c147b65195
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2023-10-29 17:49:12 +0100

    nationalities filter

---
+++ src/components/filters/nationalities/NationalitiesFilter.jsx
@@ -0,0 +1,66 @@
+import "./nationalities.css"
+import { Close } from '@mui/icons-material'
+import nationalitiesData from "../../../data/nationalities.json"
+import { useState } from "react";
+
+const NationalitiesFilter = () => {
+  const [searchText, setSearchText] = useState('');
+  const [displayedNationalities, setDisplayedNationalities] = useState([]);
+  const [selectedNationalities, setSelectedNationalities] = useState([]);
+  const [previouslySelecteNationalities, setPreviouslySelecteNationalities] = useState([]);
+
+
+const handleSearch = (e) => {
+  const searchInput = e.target.value;
+  setSearchText(searchInput);
+  
+  if(searchInput===''){
+    setDisplayedNationalities([])
+  }else{
+    const previouslySelectednationalityNames = previouslySelecteNationalities.map((nationality) => nationality);
+
+    const filtredNationalities = nationalitiesData.nationalities.filter((nationality) =>nationality.toLowerCase()
+    .includes(searchInput.toLowerCase())
+      )
+      .slice(0, 3)
+      .filter(
+        (nationality) =>!selectedNationalities.includes(nationality) && !previouslySelectednationalityNames.includes(nationality)
+      );
+  
+    setDisplayedNationalities(filtredNationalities);
+  }
+};
+const handleSelectNationality = (nationality) => {
+  setSelectedNationalities((prevSelected) => [...prevSelected, nationality]);
+  setPreviouslySelecteNationalities((prev) => [...prev, nationality]);
+  const updatedDisplayedNationalities = displayedNationalities?.filter((c) => c !== nationality);
+  setDisplayedNationalities(updatedDisplayedNationalities);
+};
+
+const handleDeselectNationality = (nationality) => {
+  setSelectedNationalities((prevSelected) =>prevSelected.filter((c) => c !== nationality));
+  setDisplayedNationalities((prevDisplayed) =>prevDisplayed.includes(nationality) ? prevDisplayed : [...prevDisplayed, nationality]);
+  setPreviouslySelecteNationalities((prevPrevSelected) =>prevPrevSelected.filter((c) => c !== nationality))};
+
+  return (
+    <div className="nationalities-container">
+      {selectedNationalities?.map(el=> 
+      <div  className='selected-nationalities'>
+        {el}<Close className='closeBtn' onClick={() => handleDeselectNationality(el)}/>
+      </div> )}
+      <div className='nationalities-item'>
+           <input className='nationalities-search' type="text" onChange={handleSearch} placeholder='__________________________________' />
+        </div>
+        {displayedNationalities?.map((nationality, i) => (
+          <div onClick={()=>handleSelectNationality(nationality)} className='nationalities-item' key={i}>
+            <span >{nationality}</span>
+          </div>
+        ))}
+            {Array(Math.max(0, 3 - displayedNationalities.length)).fill().map((_, i) => (
+                <div className='nationalities-item' key={i}> <span style={{visibility:"hidden"}}>placeholder</span></div>))}
+
+    </div>
+  )
+}
+
+export default NationalitiesFilter
