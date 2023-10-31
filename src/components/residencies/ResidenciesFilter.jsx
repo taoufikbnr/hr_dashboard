40be19eb31974b0a093ed3cdf630373a407d908a
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2023-10-31 21:36:30 +0100

    residencies fitler

--- src/components/residencies/ResidenciesFilter.jsx
+++ src/components/residencies/ResidenciesFilter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import residenciesData from "../../data/residencies.json";
 import "./residencies.css";
-import { ArrowDropDownCircleRounded } from "@mui/icons-material";
+import { ArrowDropDownCircleRounded, Close } from "@mui/icons-material";
 
 const ResidenciesFilter = () => {
   const [selectedfrenchRegions, setselectedfrenchRegions] = useState([]);
@@ -34,8 +34,32 @@ const ResidenciesFilter = () => {
       setselectedglobalRegions((prev) => [...prev, RigPost]);
     }
   };
+  const [filtredData, setfiltredData] = useState(residenciesData.countries.content);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState([]);
+  const [seach, setseach] = useState("");
+  const [selectedCountries, setSelectedCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState(residenciesData.countries.content);
+
+  const handleCountrySelect = (index) => {
+    const selectedCountry = filteredCountries[index];
+    const updatedSelectedCountries = [...selectedCountries, selectedCountry];
+    setSelectedCountries(updatedSelectedCountries);
+    setFilteredCountries((prevFilteredCountries) =>
+      prevFilteredCountries.filter((country, i) => i !== index)
+    );
+  };
+  const handleCountryDeselect = (index) => {
+    const deselectedCountry = selectedCountries[index];
+    const updatedFilteredCountries = [...filteredCountries, deselectedCountry];
+    setFilteredCountries(updatedFilteredCountries);
+    setSelectedCountries((prevSelectedCountries) =>
+      prevSelectedCountries.filter((country, i) => i !== index)
+    );
+  };
 
 
+    
   return (
     <>
     <div className="residencies-container">
@@ -93,6 +117,41 @@ const ResidenciesFilter = () => {
               {el}
             </div>
           ))}
+        </div>
+      </div>
+      <div className="residencies-menu">
+
+        <div className="country-filter">
+          <div className="country-left">
+          <span className={`residencies-header`}>
+          {residenciesData.countries.title}
+        </span>
+          {selectedCountries.map((el, i) => (
+              <div className='selected-countries' key={i}>
+                {el.country}
+                <Close className='closeBtn' onClick={() => handleCountryDeselect(i)}/>
+              </div>
+            ))}
+            <div className='country-item country-search'>
+            <input type="text" onChange={(e)=>setseach(e.target.value)} placeholder='________________________' />
+            </div>
+        {filteredCountries.filter(word=>word.country.toLocaleLowerCase().includes(seach.toLocaleLowerCase())).slice(0,1).map((el,i)=>
+          <div className={`country-item ${!seach && 'hide'}`} key={i} onClick={()=>handleCountrySelect(i)}>
+              {el.country}
+          </div>  
+          )}
+        </div>
+        <div className='country-right'>
+        <span className={`residencies-header`}>
+          {residenciesData.globalRegions.title}
+        </span>
+        {selectedCountries.map((el, i) => (
+          <div className='selected-countries' key={i}>
+              {el.location}
+            </div>
+          ))}
+          <div className="country-item"><span style={{visibility:'hidden'}} >placeholder</span></div>
+          </div>
         </div>
       </div>
     </div>
