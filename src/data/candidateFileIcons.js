68f49eef5843cdf5116359d6808c01c9db0b3f99
user <user>
2023-12-04 11:35:49 +0100

    candidate file added current icon

---
+++ src/data/candidateFileIcons.js
@@ -0,0 +1,15 @@
+import { Email_received_Empty, Email_sent_Empty, Incoming_call_Empty, LinkedIn_message_received_Empty, LinkedIn_message_sent_Empty, Outgoing_call_Empty, Physical_meeting_Empty, SMS_received_Empty, SMS_sent_Empty, Voicemail_Empty } from "./icons";
+
+
+export const canddidateFileicons ={
+Incoming_call_Empty,
+Voicemail_Empty,
+Outgoing_call_Empty,
+Email_received_Empty,
+Email_sent_Empty,
+LinkedIn_message_received_Empty,
+LinkedIn_message_sent_Empty,
+SMS_received_Empty,
+SMS_sent_Empty,
+Physical_meeting_Empty,
+}
