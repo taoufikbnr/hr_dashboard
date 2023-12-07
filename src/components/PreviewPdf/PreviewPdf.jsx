a87351d2359284bea3764afa9b1c462615924c76
taoufik-io <taoufik.bennour@esprit.tn>
2023-12-07 19:07:00 +0100

    highlight word pdf

--- src/components/PreviewPdf/PreviewPdf.jsx
+++ src/components/PreviewPdf/PreviewPdf.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Document, Page,pdfjs } from 'react-pdf'
 import "./previewPdf.css"
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
-const PreviewPdf = ({pdf}) => {
+const PreviewPdf = ({searchText,pdf}) => {
     const [numPages, setNumPages] = useState(null);
 
     const onFileLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
     };
+    function highlightPattern(text, pattern) {
+      const regex = new RegExp(pattern, 'gi');
+      return text.replace(regex, (value) => `<mark class=".high">${value}</mark>`);
+    }
+    const textRenderer = useCallback(
+      (textItem) => highlightPattern(textItem.str, searchText),
+      [searchText]
+    );
   return (
     <div className="preview-pdf">
     <Document
@@ -20,9 +31,11 @@ const PreviewPdf = ({pdf}) => {
                                 <Page
                                     key={`page_${index + 1}`}
                                     pageNumber={index + 1}
-                                    renderTextLayer={false}
+                                    // renderTextLayer={false}
                                     renderInteractiveForms={false}
                                     renderMode="canvas"
+                                    customTextRenderer={textRenderer}
+
                                 />
                             ))}
                 </Document>  
